bfcb71815f6d844f097050a9618655b9a172314e
Swampen <ole-ma@outlook.com>
2019-12-05 19:28:59 +0100

    Added description to star 1

--- Day_02/Main.py
+++ Day_02/Main.py
@@ -1,5 +1,14 @@
 
 
+"""
+99: halt
+1: adds two numbers from two positions, and stores it in a third position.
+The three integers immediately after the opcode tell you these three positions -
+the first two should be added together, the third should where the sum should be stored.
+
+2: same as 1, but with multiplication
+"""
+
 
 if __name__ == "__main__":
     print("start")

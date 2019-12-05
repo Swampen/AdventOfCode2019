f415d3a310dc506cd1cb7d5e8c522a69694340f2
Swampen <ole-ma@outlook.com>
2019-12-05 22:49:44 +0100

    Almost completed star 1

--- Day_02/Main.py
+++ Day_02/Main.py
@@ -1,4 +1,4 @@
-
+import operator as op
 
 """
 99: halt
@@ -8,7 +8,31 @@ the first two should be added together, the third should where the sum should be
 
 2: same as 1, but with multiplication
 """
+operator = { 1: op.add, 2: op.mul}
 
+def restore_gravity(ints):
+    pos = 0
+
+    ints[1] = 12
+    ints[2] = 2
+
+    while ints[pos] != 99:
+        print(ints)
+        action = ints[pos]
+        first = ints[pos+1]
+        second = ints[pos+2]
+        output = ints[pos+3]
+
+        result = operator[action](first, second)
+        ints[output] = result
+
+        pos+=4
+
+    return ints[0]
 
 if __name__ == "__main__":
-    print("start")
+    with open("input.txt") as input:
+        strings = input.read().split(",")
+        ints = [int(i) for i in strings]
+
+    print(restore_gravity(ints))
